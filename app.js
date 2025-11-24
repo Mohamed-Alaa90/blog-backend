200282d820a967701748d18938e010021d5c3fe3
Mohamed_Alaa <m7md3laa500@gmail.com>
2025-11-24 05:32:54 +0200

    Refactor app.js to set up static files and EJS layout engine

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 
-const PORT = 5000 || process.env;
+app.use(express.static("public"));
 
-app.get("", (res) => {
+//Layouts
+const expressLayout = require("express-ejs-layouts");
+app.use(expressLayout);
+app.set("layout", "layouts/main");
+app.set("view engine", "ejs");
+
+app.get("", (res, req) => {
   res.send("Hello");
 });
 
+
+const PORT = 5000 || process.env;
 app.listen(PORT, () => {
   console.info("APP listen In Port", PORT);
 });
