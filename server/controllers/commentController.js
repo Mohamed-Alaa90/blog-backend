0375193f46308c107aac396c5fa032584e23177c
Mohamed Alaa <m7m3laa500@gmail.com>
2026-01-24 02:44:09 +0200

    feat: Implement createComment functionality with validation and user association

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,4 +1,6 @@
 import Comment from "../models/Comment.js";
+import User from "../models/User.js";
+
 import {
   commentCreateValidation,
   commentUpdateValidation,
@@ -6,4 +8,26 @@ import {
 import path from "path";
 import asyncHandler from "express-async-handler";
 
-export const addComment = asyncHandler(async (req, res) => {});
+export const createComment = asyncHandler(async (req, res) => {
+  const { error } = commentCreateValidation(req.body);
+  if (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.details[0].message,
+    });
+  }
+
+  const profile = await User.findById(req.user.id);
+
+  const comment = await Comment.create({
+    postId: req.body.postId,
+    text: req.body.text,
+    user: req.user.id,
+    username: profile.username,
+  });
+
+  res.status(201).json({
+    success: true,
+    comment,
+  });
+});
--- server/routes/commentRoute.js
+++ server/routes/commentRoute.js
@@ -1,6 +1,9 @@
 import { validateObjectId } from "../middlewares/validateObjectId.js";
 import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.js";
+import { createComment } from "../controllers/commentController.js";
 const router = Router();
+
+router.route("/").post(verifyToken, createComment);
 
 export default router;
